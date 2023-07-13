507474d9215636a708e00d3c687d5bee59b37928
Jackx333 <adhamt79@gmai.com>
2023-07-13 14:39:34 -0700

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char: %d bytes(s) \n,sizeof(char));
+	printf("size of int: %d bytes(s) \n,sizeof(int));
+	printf("size of long int: %d bytes(s) \n,sizeof(long int));
+	printf("size of long long int: %d bytes(s) \n,sizeof(long long int));
+	printf("size of float: %d bytes(s) \n,sizeof(float));
+	return(0);
+}
