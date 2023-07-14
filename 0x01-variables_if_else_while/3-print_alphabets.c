cfc27b65053cb659ed4902786337c91950163649
Jackx333 <adhamt79@gmai.com>
2023-07-14 15:12:43 -0700

    trisl

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int low_letter;
+	int up_letter;
+
+	low_letter = 'a';
+	up_letter = 'A';
+	while(low_letter <= 'z')
+	{
+		putchar (low_letter);
+		low_letter++;
+		return (0);
+	}
+	while (up_letter <= 'Z')
+	{
+		putchar (up_letter);
+		up_letter++;
+		return (0);
+	}
+	putchar('\n');
+}
