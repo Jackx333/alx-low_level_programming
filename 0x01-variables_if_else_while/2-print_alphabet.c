413f15a6993105a0bf61d696541b36abfaca6803
Jackx333 <adhamt79@gmai.com>
2023-07-14 14:13:16 -0700

    second task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	for (int letter = 'a'; letter <='z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
